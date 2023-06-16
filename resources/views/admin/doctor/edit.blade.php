0b3ab82a1b4c3dcc8a4e686679efb8635b3411c5
Alexander <aleks.mackeencko@yandex.ru>
2023-06-17 02:14:11 +0500

    changed CRUD views for admine doctor

--- resources/views/admin/doctor/edit.blade.php
+++ resources/views/admin/doctor/edit.blade.php
@@ -1,49 +1,48 @@
 @extends('layouts.app')
+
 @section('content')
+
 <link rel="stylesheet" href="{{ asset('css/userEdit.css') }}">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-<div>
-    <h1>Редактирование Врача</h1>
-    @if ($errors->any())
-        <div>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    <form method="POST" action="{{ route('admin.doctor.update', $doctor->id) }}">
-        @csrf
-        @method('patch')
-        {{-- <div>
-            <label for="name">ФИО:</label>
-            <input type="text" id="name" name="name" value="{{ old('name', $doctor->user->name) }}" required autofocus>
-        </div> --}}
-        <div>
-            <p>{{ $doctor->user->name }}</p>
-        </div>
-        {{-- <div>
-            <label for="name">ФИО:</label>
-            <input type="text" id="name" name="name" value="{{ $doctor->user->name }}" readonly>
-        </div> --}}
-
-        <div class="form-group">
-            <label for="specializations">Специализация</label>
-            <select multiple class="form-control" id="specializations" name="specializations[]">
-                @foreach ($specializations as $specialization)
-                    <option
-                    @foreach ($doctor->specializations as $doctorSpecialization)
-                        {{$specialization->id === $doctorSpecialization->id ? 'selected' : ''}}
+<div class="container mt-5">
+    <div class="card shadow-sm p-3 mb-5 bg-body rounded" style="max-width: 500px; margin: 0 auto;">
+        <h1 class="text-center mb-4">Редактирование Врача</h1>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
                     @endforeach
+                </ul>
+            </div>
+        @endif
+        <form method="POST" action="{{ route('admin.doctor.update', $doctor->id) }}">
+            @csrf
+            @method('patch')
+            <div class="mb-3">
+                <div class="card shadow-sm p-3 mb-3 bg-body rounded text-center">
+                    {{ $doctor->user->name }}
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="specializations">Специализация:</label>
+                <select multiple class="form-control" id="specializations" name="specializations[]">
+                    @foreach ($specializations as $specialization)
+                        <option
+                            @foreach ($doctor->specializations as $doctorSpecialization)
+                                {{ $specialization->id === $doctorSpecialization->id ? 'selected' : '' }}
+                            @endforeach
+                            value="{{ $specialization->id }}">{{ $specialization->name }}</option>
+                    @endforeach
+                </select>
+            </div>
 
-                    value="{{$specialization->id}}">{{$specialization->name}}</option>
-                @endforeach
-            </select>
-          </div>
-        <button type="submit">Обновить</button>
-    </form>
-    <a href="{{ route('admin.doctor.index') }}">Обратно к списку пользователей</a>
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary">Обновить</button>
+            </div>
+            <a href="{{ route('admin.doctor.index') }}" class="btn btn-secondary">Назад</a>
+        </form>
+    </div>
 </div>
 @endsection
--- resources/views/admin/doctor/index.blade.php
+++ resources/views/admin/doctor/index.blade.php
@@ -1,22 +1,20 @@
 @extends('layouts.app')
 @section('content')
+
 <link rel="stylesheet" href="{{ asset('css/usersTable.css') }}">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-
 <div class="container">
     <div class="row justify-content-center">
-        {{-- <div class="col-md-8"> --}}
+        <div class="col-md-12">
             <h1>Управление врачами</h1>
-            <div>
-                <a href="{{route('admin.doctor.create')}}" class="btn btn-primary mb-3">Добавить нового</a>
-            </div>
-            <table>
+            <table class="table">
                 <thead>
                     <tr>
                         <th>ФИО</th>
                         <th>Специализация</th>
-                        <th>Действия</th>
+                        {{-- <th>Действия</th> --}}
+                        {{-- <th>Действия</th> --}}
                     </tr>
                 </thead>
                 <tbody>
@@ -26,8 +24,11 @@
                             <td>{{  json_decode(($doctor->specializations->pluck('name')),true)[0]}}</td>
                             <td>
                                 <div>
-                                    <button type="button" class="btn btn-light"><a href="{{route('admin.doctor.show', $doctor->id)}}">Посмотреть</a></button>
+                                    <a href="{{ route('admin.doctor.show', $doctor->id) }}" class="btn btn-primary btn-sm">Просмотр</a>
                                 </div>
+                                {{-- <div>
+                                    <button type="button" class="btn btn-light"><a href="{{route('admin.doctor.show', $doctor->id)}}">Посмотреть</a></button>
+                                </div> --}}
                                 <div>
                                     <form method="POST" action="{{route('admin.doctor.delete', $doctor->id)}}">
                                         @csrf
@@ -40,15 +41,13 @@
                     @endforeach
                 </tbody>
             </table>
-
-        {{-- </div> --}}
+            <div class="text-center">
+                <a href="{{ route('home') }}" class="btn btn-primary text-white">Назад</a>
+            </div>
+            <div class="mt-3">
+                {{$doctors->links()}}
+            </div>
+        </div>
     </div>
-</div>
-{{-- <div> --}}
-    <hr>
-    <button type="button" class="btn btn-light"><a href="{{ route('home') }}">Вернуться в кабинет</a></button>
-{{-- </div> --}}
-<div class="mt-3">
-    {{$doctors->links()}}
 </div>
 @endsection
